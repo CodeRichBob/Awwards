be2ec8bc824fbc018ccee1e36046fa4ec5c88609
Robert <rgitonga.07@gmail.com>
2022-06-14 04:15:11 +0300

    ft:Add url for rating a project

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('profile/update/',views.update_profile,name='profileUpdate'),
     path('post_project/',views.post_project,name='post_project'),
     path('search/project/',views.search_project,name='search_project'),
+    path('rate_project/<int:project_id>',views.rate_project,name='rate'),
 ]
