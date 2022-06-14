d8233650d1c8ad74e657d8d4aec5119dda478cdc
Robert <rgitonga.07@gmail.com>
2022-06-14 03:54:08 +0300

    ft:show profile while logged in

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('profile/',views.profile,name='profile'),
     path('profile/update/',views.update_profile,name='profileUpdate'),
+    path('post_project/',views.post_project,name='post_project'),
+    path('search/project/',views.search_project,name='search_project'),
 ]
--- Awwards/views.py
+++ Awwards/views.py
@@ -7,3 +7,10 @@ def home(request):
   projects = Projects.objects.all() #obtaining all posted projects
 
   return render(request,'index.html',{"projects":projects})
+
+def profile(request): 
+  '''Function rendering a logged in user's profile page'''
+  profile = Profile.objects.filter(user=request.user).first()
+  user_projects = Projects.objects.filter(project_owner=request.user).all()
+  return render(request,'registration/profile.html',{"profile":profile,"user_projects":user_projects})
+
