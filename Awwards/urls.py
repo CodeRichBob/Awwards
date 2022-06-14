54cd053d3d090962fb1bcb1b5b53b8f722aa5998
Robert <rgitonga.07@gmail.com>
2022-06-14 04:20:40 +0300

    ft:Create url for view a project's details

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post_project/',views.post_project,name='post_project'),
     path('search/project/',views.search_project,name='search_project'),
     path('rate_project/<int:project_id>',views.rate_project,name='rate'),
+    path('details/project/<int:project_id>',views.project_details,name='project_details'),
 ]
